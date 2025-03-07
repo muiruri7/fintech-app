157ebc26f5638c95ba8bfeb7897f3344552b4977
muiruri7 <leon.w.muiruri@gmail.com>
2025-03-07 08:09:24 +0300

    ok ok

--- src/app/modules/transaction/transaction.module.ts
+++ src/app/modules/transaction/transaction.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionFormComponent } from './transaction-form/transaction-form.component';
 import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
+import { RouterModule } from '@angular/router'; // ✅ Fix routerLink error
 
+// ✅ Correctly import Angular Material Modules
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,8 +17,11 @@ import { TransactionDetailsComponent } from './transaction-details/transaction-d
     TransactionDetailsComponent
   ],
   imports: [
-    CommonModule,
-    TransactionRoutingModule,
+    CommonModule,     
+    RouterModule,     
+    MatTableModule,  
+    MatButtonModule,  
+    MatIconModule
   ]
 })
 export class TransactionModule { }
